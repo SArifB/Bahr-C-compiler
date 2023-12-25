a87ab3776c3af6bcfac50c416a7b237b39fcca05
sarifb <saye.arif@gmail.com>
2023-12-25 21:05:25 +0200

    Removed unncessary type files

--- src/parser/types.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <parser/mod.h>
-
-extern bool is_numeric(Type* type);
-extern void add_type(Node* node);
-extern Type* copy_type(Type* type);
-extern Type* make_ptr_type(Type* base);
-extern Type* make_decl_type(TypeKind kind);
-extern Type* make_fn_type(Type* args, Type* ret);
