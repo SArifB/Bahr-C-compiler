354efc01c6f577db9aee70b421ba1cb9e2919891
sarifb <saye.arif@gmail.com>
2023-12-30 21:11:54 +0200

    Updated example test

--- test_run.sh
+++ test_run.sh
@@ -4,7 +4,7 @@
 # The project needs to be built first
 # Requires: fish, clang, gcc
 # Can substitute fish for any shell and gcc for any compiler
-./build/bin test/src/test.bh
+./build/bin test/src/test.bh > test/out/test.ll
 cd test/out/
 llc test.ll
 gcc -o test main.c test.s
