fffe2facf8a63e8b54fc9c24e3a0320c9d5a00ed
sarifb <saye.arif@gmail.com>
2024-01-03 16:44:54 +0200

    Removed unnecessary files

--- src/engine/mod.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "codegen/mod.h"
-#include <llvm-c/ExecutionEngine.h>
-
-LLVMExecutionEngineRef engine_make(Codegen* cdgn);
-void engine_shutdown(LLVMExecutionEngineRef engine);
