190e3a5b0ce54a0204a1c091eb0e7c4b29f2552e
Arif Basharyar <arif.basharyar@tuni.fi>
2024-02-25 18:34:18 +0200

    Implemented open addressing hashmap. Experimenting type erasure. Requires benchmarking.

--- src/hashmap/mod.c
+++ src/hashmap/mod.c
@@ -4,60 +4,208 @@
 #include <string.h>
 #include <utility/mod.h>
 
-HashMap* hashmap_make(usize size) {
-  usize final_size = 2;
+#define LINKED_LIST 0
+#define OPEN_ADDRESSING 1
+
+#define MAP_TYPE OPEN_ADDRESSING
+#if MAP_TYPE == LINKED_LIST
+
+typedef struct HashNode HashNode;
+struct HashNode {
+  HashNode* next;
+  void* value;
+  usize key;
+};
+
+typedef struct HashTable HashTable;
+struct HashTable {
+  usize size;
+  HashNode* entries[];
+};
+
+#elif MAP_TYPE == OPEN_ADDRESSING
+
+typedef struct HashNode HashNode;
+struct HashNode {
+  void* value;
+  usize key;
+};
+
+typedef struct HashTable HashTable;
+struct HashTable {
+  usize size;
+  usize count;
+  HashNode entries[];
+};
+
+#endif
+
+static inline usize get_hash(usize hash, const u8* bytes, usize length);
+static HashTable* alloc_table(usize size);
+static HashNode* get_entry(HashTable* table, usize hash);
+
+HashMap hashmap_make(usize size) {
+  usize final_size = 4;
   while (final_size < size) {
     final_size *= 2;
   }
-  HashMap* map = calloc(1, sizeof(HashMap) + sizeof(HashMapNode*) * final_size);
-  if (map == nullptr) {
-    perror("calloc");
-    exit(1);
-  }
-  map->size = final_size;
-  return map;
+  HashTable* table = alloc_table(final_size);
+  return (HashMap){table};
 }
 
-void hashmap_free(HashMap* map) {
-  HashMapNode* cursor = nullptr;
-  for (usize i = 0; i != map->size; ++i) {
-    cursor = map->buckets[i];
+static inline usize get_index(usize index, usize size) {
+  return index & (size - 1);
+}
+
+#if MAP_TYPE == LINKED_LIST
+
+static HashNode* alloc_node(usize hash);
+static HashNode* insert_node(HashTable* table, usize hash);
+
+void hashmap_free(HashMap map) {
+  for (usize i = 0; i != map.table->size; ++i) {
+    HashNode* cursor = map.table->entries[i];
     while (cursor != nullptr) {
-      HashMapNode* tmp = cursor;
+      HashNode* tmp = cursor;
       cursor = cursor->next;
       free(tmp);
     }
   }
-  free(map);
+  free(map.table);
 }
 
-static inline usize write_hash(usize hash, const u8* bytes, usize length);
-
 void** hashmap_get(HashMap* map, StrView key) {
-  usize hash = write_hash(0, (u8*)key.ptr, key.size);
-  usize index = hash & (map->size - 1);
-  HashMapNode** cursor = &map->buckets[index];
+  HashTable* table = map->table;
+  usize hash = get_hash(0, (u8*)key.ptr, key.size);
 
-  while (*cursor != nullptr) {
-    HashMapNode* latest = *cursor;
-    if (latest->key_hash == hash) {
-      return &latest->value;
-    }
-    cursor = &latest->next;
+  HashNode* entry = get_entry(table, hash);
+  if (entry != nullptr) {
+    return &entry->value;
   }
+  entry = insert_node(table, hash);
+  return &entry->value;
+}
 
-  HashMapNode* node = malloc(sizeof(HashMapNode));
+void** hashmap_find(HashMap* map, StrView key) {
+  HashTable* table = map->table;
+  usize hash = get_hash(0, (u8*)key.ptr, key.size);
+  HashNode* entry = get_entry(table, hash);
+  if (entry != nullptr) {
+    return &entry->value;
+  }
+  return nullptr;
+}
+
+static HashTable* alloc_table(usize size) {
+  HashTable* table = calloc(1, sizeof(HashMap) + sizeof(HashNode*) * size);
+  if (table == nullptr) {
+    perror("calloc");
+    exit(1);
+  }
+  table->size = size;
+  return table;
+}
+
+static HashNode* alloc_node(usize hash) {
+  HashNode* node = malloc(sizeof(HashNode));
   if (node == nullptr) {
     perror("malloc");
     exit(1);
   }
-  *node = (HashMapNode){
-    .key_hash = hash,
-  };
-
-  *cursor = node;
-  return &node->value;
+  *node = (HashNode){.key = hash};
+  return node;
 }
+
+static HashNode* get_entry(HashTable* table, usize hash) {
+  usize index = get_index(hash, table->size);
+  HashNode* entry = table->entries[index];
+  for (; entry != nullptr; entry = entry->next) {
+    if (entry->key == hash) {
+      break;
+    }
+  }
+  return entry;
+}
+
+static HashNode* insert_node(HashTable* table, usize hash) {
+  usize index = get_index(hash, table->size);
+  HashNode* new_entry = alloc_node(hash);
+  new_entry->next = table->entries[index];
+  table->entries[index] = new_entry;
+  return new_entry;
+}
+
+#else
+
+static HashTable* resize(HashTable* old_table);
+
+void hashmap_free(HashMap map) {
+  free(map.table);
+}
+
+void** hashmap_get(HashMap* map, StrView key) {
+  HashTable* table = map->table;
+  if (table->count == table->size) {
+    map->table = resize(table);
+    table = map->table;
+  }
+  usize hash = get_hash(0, (u8*)key.ptr, key.size);
+  HashNode* entry = get_entry(table, hash);
+  if (entry->key != 0) {
+    return &entry->value;
+  }
+  table->count += 1;
+  entry->key = hash;
+  return &entry->value;
+}
+
+void** hashmap_find(HashMap* map, StrView key) {
+  usize hash = get_hash(0, (u8*)key.ptr, key.size);
+  HashNode* entry = get_entry(map->table, hash);
+  if (entry->key != 0) {
+    return &entry->value;
+  }
+  return nullptr;
+}
+
+static HashTable* alloc_table(usize size) {
+  HashTable* map = calloc(1, sizeof(HashTable) + sizeof(HashNode) * size);
+  if (map == nullptr) {
+    perror("calloc");
+    exit(1);
+  }
+  map->size = size;
+  return map;
+}
+
+static HashNode* get_entry(HashTable* table, usize hash) {
+  usize index = get_index(hash, table->size);
+  HashNode* cursor = table->entries + index;
+
+  while (cursor->key != 0) {
+    if (cursor->key == hash) {
+      break;
+    }
+    index = get_index(index + 1, table->size);
+    cursor = table->entries + index;
+  }
+  return cursor;
+}
+
+static HashTable* resize(HashTable* old_table) {
+  HashTable* new_table = alloc_table(old_table->size * 2);
+  HashNode* iter = old_table->entries;
+  HashNode* sentinel = old_table->entries + old_table->size;
+
+  for (; iter != sentinel; ++iter) {
+    *get_entry(new_table, iter->key) = *iter;
+  }
+  new_table->count = old_table->count;
+  free(old_table);
+  return new_table;
+}
+
+#endif
 
 #if __SIZEOF_POINTER__ == 8
 #define K 0x517cc1b727220a95UL
@@ -65,7 +213,7 @@ void** hashmap_get(HashMap* map, StrView key) {
 #define K 0x9e3779b9UL
 #endif
 
-static inline usize rotate_left(usize value, int count) {
+static inline usize rotate_left(usize value, i32 count) {
   return (value << count) | (value >> (sizeof(usize) * 8 - count));
 }
 
@@ -75,23 +223,23 @@ static inline usize add_to_hash(usize hash, usize i) {
   return hash;
 }
 
-static inline usize write_hash(usize hash, const u8* bytes, usize length) {
+static inline usize get_hash(usize hash, const u8* bytes, usize length) {
   while (length >= sizeof(usize)) {
-    usize val = {};
+    usize val = 0;
     memcpy(&val, bytes, sizeof(usize));
     hash = add_to_hash(hash, val);
     bytes += sizeof(usize);
     length -= sizeof(usize);
   }
   if (length >= 4) {
-    uint32_t val = 0;
+    u32 val = 0;
     memcpy(&val, bytes, 4);
     hash = add_to_hash(hash, val);
     bytes += 4;
     length -= 4;
   }
   if (length >= 2) {
-    uint32_t val = 0;
+    u32 val = 0;
     memcpy(&val, bytes, 2);
     hash = add_to_hash(hash, val);
     bytes += 2;
--- src/hashmap/mod.h
+++ src/hashmap/mod.h
@@ -1,19 +1,12 @@
 #pragma once
 #include <utility/mod.h>
 
-typedef struct HashMapNode HashMapNode;
-struct HashMapNode {
-  HashMapNode* next;
-  void* value;
-  usize key_hash;
-};
-
 typedef struct HashMap HashMap;
 struct HashMap {
-  usize size;
-  HashMapNode* buckets[];
+  void* table;
 };
 
-extern HashMap* hashmap_make(usize size);
-extern void hashmap_free(HashMap* map);
+extern HashMap hashmap_make(usize size);
+extern void hashmap_free(HashMap map);
 extern void** hashmap_get(HashMap* map, StrView key);
+extern void** hashmap_find(HashMap* map, StrView key);
